054a11bc4b2abd347186a82b82f52f0e2676e5f0
Jasmine May Darman <65880499+Linocut@users.noreply.github.com>
2023-04-30 03:50:13 -0400

    Backup branch push for general code that needs to be transferred
    Code to copy:
    bubble shield2
    light follow

---
+++ Assets/Scripts/BubbleShield2.cs
@@ -0,0 +1,77 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class BubbleShield2 : MonoBehaviour
+
+{
+    public GameObject shield;
+    public float shieldTime;
+    public int shieldCharge;
+    public AudioSource source;
+    public Material normalMaterial;
+    public Material flickerMaterial;
+
+    private PlayerHealth playerHealth;
+    private bool chargeCheck;
+    private Renderer shieldRenderer; 
+
+    public void IncreaseTime(float amount)
+    {
+        shieldTime += amount;
+    }
+
+    void OnEnable()
+    {
+        playerHealth = GetComponent<PlayerHealth>();
+        shieldRenderer = shield.GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("BubbleShield"))
+        {
+            chargeCheck = playerHealth.ChargeCheck(shieldCharge);
+
+            if (chargeCheck)
+            {
+                playerHealth.UseCharge(shieldCharge);
+
+                if (source != null)
+                {
+                    source.Play();
+                }
+                shield.gameObject.SetActive(true);
+                StartCoroutine(FlickerShield(shieldTime - 1f)); // start the flicker Coroutine with a delay of 1 second before the shield expires
+                Invoke("DownShield", shieldTime);
+            }
+        }
+    }
+
+    IEnumerator FlickerShield(float remainingTime)
+    {
+        float interval = 0.1f;
+        float timeFactor = 1.0f;
+
+        while (remainingTime > 0f)
+        {
+            if (remainingTime < 3f)
+            {
+                timeFactor = Mathf.Sin(20f * Time.time) + 1.0f;
+            }
+            Color flickerColor = Color.Lerp(normalMaterial.color, flickerMaterial.color, timeFactor);
+            shieldRenderer.material.color = flickerColor;
+            yield return new WaitForSeconds(interval);
+            remainingTime -= interval;
+        }
+        shieldRenderer.material = normalMaterial;
+    }
+
+    void DownShield()
+    {
+        shield.gameObject.SetActive(false);
+    }
+}
